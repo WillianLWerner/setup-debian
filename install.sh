75f7018a0d347407724fc99f4a0f7ef1fbcd1217
Willian Werner <112822572+WillianLWerner@users.noreply.github.com>
2024-08-07 12:34:28 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,32 +1,23 @@
 #!/bin/bash
 set -e
 
-# Adiciçã da arquitetura 32 bits ao sistema
-dpkg --add-architecture i386
-apt update
+# Adição de repositórios adicionais do Debian
+apt_list="/etc/apt/sources.list"
+echo "# Ativando o repositório de Backports" >> "$apt_list"
+echo "deb http://deb.debian.org/debian ${VERSION_CODENAME}-backports main" >> "$apt_list"
+echo "deb-src http://deb.debian.org/debian ${VERSION_CODENAME}-backports main" >> "$apt_list"
+echo "# Ativando o repositório ftp" >> "$apt_list"
+echo "deb http://ftp.debian.org/debian ${VERSION_CODENAME} main" >> "$apt_list"
+echo "deb http://ftp.br.debian.org/debian ${VERSION_CODENAME} main" >> "$apt_list"
+
+# Atualização dos pacotes
+sudo apt update
 
 # Ativação da configuração de completar o nome dos pacotes na instalação via apt
 apt install bash-completion
 bashrc="/etc/bash.bashrc"
 sed -i '35,41s/^#//' "$bashrc"
 source "$bashrc"
-
-# Instalação do pacote sudo no sistema
-apt install sudo
-adduser root sudo
-adduser willian sudo
-
-# Instalação do pacote para o regenciamento da rede pelo terminal
-apt install net-tools
-
-# Ativação do programa TRIM para seu SSD
-#trim="/etc/cron.daily/trim"
-#echo '#!/bin/sh' > "$trim"
-#echo 'LOG=/var/log/trim.log' >> "$trim"
-#echo 'echo "*** $(date -R) ***" >> $LOG' >> "$trim"
-#echo 'fstrim -v / >> $LOG' >> "$trim"
-#echo 'fstrim -v /home >> $LOG' >> "$trim"
-#chmod +x /etc/cron.daily/trim
 
 # Configuração dos parâmetros de swapiness e cache
 sysctl="/etc/sysctl.conf"
